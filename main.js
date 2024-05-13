0c9bcf0a70b093185fcc36438ce5722d36d50572
Ghurir <39923434+Ghurir@users.noreply.github.com>
2024-05-13 03:16:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+var topnav = ID("topnav");
+var pages = topnav.childNodes;
+var currentPageName = "";
+
+var aboutPage = ID("About");
+var sourcesPage = ID("Sources");
+var toolPage = ID("BlockTool");
+var trackerPage = ID("Tracker");
+
+var contentFrame = ID("contentFrame");
+
+oninit();
+
+function oninit(){
+	updatePage();
+	addEventListener("hashchange", hashchanged);
+}
+
+function ID(id){
+	return document.getElementById(id);
+}
+
+function activePage(){
+	currentPageName = location.hash.replace("#","");
+	ID(currentPageName).classList.add("active");
+}
+
+function hashchanged(event){
+	updatePage();
+};
+
+function disableAllPages(){
+	pages.forEach(a=>a.classList&&a.classList.remove("active"));
+}
+
+function updatePage(){
+	disableAllPages();
+	activePage();
+	contentFrame.src = `./pages/${currentPageName}/index.html`;
+}
